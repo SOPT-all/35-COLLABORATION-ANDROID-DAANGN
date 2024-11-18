b74b6e60095e6916ee62f7b7e6084aece09b8195
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-11-18 14:13:35 +0900

    [Add/#1] DummyService 추가

---
+++ app/src/main/java/org/sopt/carrot/data/Service/DummyService.kt
@@ -0,0 +1,13 @@
+package org.sopt.carrot.data.Service
+
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface DummyService {
+    //세미나 예시입니다.
+
+//    @POST("/user")
+//    suspend fun postSignUp(
+//        @Body request: RequestSignUpDto
+//    ): BaseResponse<ResponseSignUpDto>
+}
