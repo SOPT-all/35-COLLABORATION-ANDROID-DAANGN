3413bbb9433e3319a161c74e76ccd423f501d934
MinJae Han <hanminjae131@gmail.com>
2024-11-22 07:56:29 +0900

    [Add/#9] TitleSearchScreen 경로 추가

--- app/src/main/java/org/sopt/carrot/presentation/NavHost.kt
+++ app/src/main/java/org/sopt/carrot/presentation/NavHost.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import org.sopt.carrot.presentation.ExampleScreen1.ExampleScreen1
 import org.sopt.carrot.presentation.ExampleScreen2.ExampleScreen2
+import org.sopt.carrot.presentation.titleSearchScreen.TitleSearchScreen
 
 @Composable
 fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) {
@@ -15,6 +16,14 @@ fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) 
         startDestination = "example_screen_1",
         modifier = modifier
     ) {
+        composable(ScreenRoutes.TITLE_SEARCH) {
+            TitleSearchScreen(
+                onBackClick = { navController.popBackStack() },
+                onProductClick = { productId ->
+                    navController.navigate(ScreenRoutes.EXAMPLE_SCREEN_2)
+                }
+            )
+        }
         composable(ScreenRoutes.EXAMPLE_SCREEN_1) { ExampleScreen1(navController) }
         composable(ScreenRoutes.EXAMPLE_SCREEN_2) { ExampleScreen2(navController) }
     }
--- app/src/main/java/org/sopt/carrot/presentation/ScreenRoute.kt
+++ app/src/main/java/org/sopt/carrot/presentation/ScreenRoute.kt
@@ -1,6 +1,7 @@
 package org.sopt.carrot.presentation
 
 object ScreenRoutes {
+    const val TITLE_SEARCH = "title_search"
     const val EXAMPLE_SCREEN_1 = "example_screen_1"
     const val EXAMPLE_SCREEN_2 = "example_screen_2"
 }
