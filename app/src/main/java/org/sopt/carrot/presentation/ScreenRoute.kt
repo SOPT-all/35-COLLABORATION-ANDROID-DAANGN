a51d9779eea99dc0d29638820b68922bc801209c
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-11-24 19:48:34 +0900

    [Add/#11] ScreenRoute에 MainScreen 추가

--- app/src/main/java/org/sopt/carrot/presentation/ScreenRoute.kt
+++ app/src/main/java/org/sopt/carrot/presentation/ScreenRoute.kt
@@ -5,4 +5,5 @@ object ScreenRoutes {
     const val EXAMPLE_SCREEN_1 = "example_screen_1"
     const val EXAMPLE_SCREEN_2 = "example_screen_2"
     const val CATEGORY_SCREEN = "category_screen"
+    const val MAIN_SCREEN = "main_screen"
 }
