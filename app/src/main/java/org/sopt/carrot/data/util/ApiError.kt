d9762a2764251596c52cb09277e2d83516ffd4f0
Hyoeun <joohyoeun@sookmyung.ac.kr>
2024-11-27 00:38:04 +0900

    [Feat/#17] Home 서버통신

---
+++ app/src/main/java/org/sopt/carrot/data/util/ApiError.kt
@@ -0,0 +1,9 @@
+package org.sopt.carrot.data.util
+
+import kotlinx.serialization.json.Json
+import org.sopt.carrot.data.dto.response.BaseResponse
+import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+
