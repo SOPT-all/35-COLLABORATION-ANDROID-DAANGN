17859b7a6244f1fd6c646f768371fa5803a2f706
MinJae Han <hanminjae131@gmail.com>
2024-11-28 15:56:58 +0900

    [Mod/#25] user api 관심사 분리

--- app/src/main/java/org/sopt/carrot/data/service/ProductService.kt
+++ app/src/main/java/org/sopt/carrot/data/service/ProductService.kt
@@ -3,7 +3,6 @@ package org.sopt.carrot.data.service
 import org.sopt.carrot.data.model.response.BaseResponse
 import org.sopt.carrot.data.model.response.ResponseProductInfoDto
 import org.sopt.carrot.data.model.response.ResponseRelatedProductsDto
-import org.sopt.carrot.data.model.response.ResponseSearchDto
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
