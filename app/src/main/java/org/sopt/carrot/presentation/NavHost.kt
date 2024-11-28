aaabcd2f31ca05f4290d4cb550983dedc80a0cfe
wjdrjs00 <kimjk3409@gmail.com>
2024-11-29 05:32:16 +0900

    [Mod#23] 네비게이션 화면 전환 로직 수정

--- app/src/main/java/org/sopt/carrot/presentation/NavHost.kt
+++ app/src/main/java/org/sopt/carrot/presentation/NavHost.kt
@@ -1,6 +1,5 @@
 package org.sopt.carrot.presentation
 
-import android.net.Uri
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -8,12 +7,8 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import androidx.navigation.navArgument
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 import org.sopt.carrot.presentation.ExampleScreen1.ExampleScreen1
 import org.sopt.carrot.presentation.ExampleScreen2.ExampleScreen2
-import org.sopt.carrot.presentation.titleSearchScreen.TitleSearchScreen
 import org.sopt.carrot.presentation.category.CategoryScreen
 import org.sopt.carrot.presentation.main.MainScreen
 import org.sopt.carrot.presentation.productDetailScreen.ProductDetailScreen
@@ -27,9 +22,8 @@ fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) 
         startDestination = ScreenRoutes.MAIN_SCREEN,
         modifier = modifier
     ) {
-        composable(ScreenRoutes.TITLE_SEARCH) {
-            TitleSearchScreen(navController)
-        }
+        composable(ScreenRoutes.TITLE_SEARCH) { TitleSearchScreen(navController) }
+
         composable(
             route = ScreenRoutes.PRODUCT_DETAIL_WITH_ARGS,
             arguments = listOf(
@@ -39,35 +33,24 @@ fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier) 
         ) {
             ProductDetailScreen(navController)
         }
+
         composable(ScreenRoutes.PRODUCT_DETAIL) { ProductDetailScreen(navController) }
+
         composable(ScreenRoutes.MAIN_SCREEN) { MainScreen(navController) }
-        composable(ScreenRoutes.CATEGORY_SCREEN) {
 
         composable(ScreenRoutes.EXAMPLE_SCREEN_1) { ExampleScreen1(navController) }
         composable(ScreenRoutes.EXAMPLE_SCREEN_2) { ExampleScreen2(navController) }
 
-        /* 이런식으로 사용하면 됩니다!
-        composable(
-            route = "${ScreenRoutes.EXAMPLE_SCREEN_2}/{json}",
-            arguments = listOf(
-                navArgument("json") {
-                    type = NavType.StringType
-                    nullable = true
-                }
-            )
-        ) { backStackEntry ->
-            val json = backStackEntry.arguments?.getString("json")
-            val selectedCategories: List<String> = json?.let {
-                Json.decodeFromString(it)
-            } ?: emptyList()
-            ExampleScreen2(navController, selectedCategories)
-        }
-        */
-
         composable(route = ScreenRoutes.CATEGORY_SCREEN) {
             CategoryScreen(
                 onBackClick = { navController.popBackStack() },
-                navigateToHome = { navController.navigate(ScreenRoutes.EXAMPLE_SCREEN_2) }
+                navigateToHome = { selectedCategories ->
+                    navController.currentBackStackEntry?.savedStateHandle?.set(
+                        key = "selectedCategories",
+                        value = selectedCategories
+                    )
+                    navController.navigate(ScreenRoutes.MAIN_SCREEN)
+                }
             )
         }
 
