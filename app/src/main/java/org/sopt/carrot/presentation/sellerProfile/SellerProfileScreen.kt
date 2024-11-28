75a5b8f95bfc3e2d56288385e241bc2a5c07df3b
wjdrjs00 <kimjk3409@gmail.com>
2024-11-28 16:40:32 +0900

    [Mod/#15] 하단 여백 패딩 수정

--- app/src/main/java/org/sopt/carrot/presentation/sellerProfile/SellerProfileScreen.kt
+++ app/src/main/java/org/sopt/carrot/presentation/sellerProfile/SellerProfileScreen.kt
@@ -1,11 +1,11 @@
 package org.sopt.carrot.presentation.sellerProfile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import org.sopt.carrot.presentation.sellerProfile.component.SellerFollowDialog
 import org.sopt.carrot.presentation.sellerProfile.component.SellerProfileTopBar
 import org.sopt.carrot.presentation.sellerProfile.component.section.BadgeSection
@@ -29,39 +30,41 @@ fun SellerProfileScreen() {
 
     var isDialogVisible by remember { mutableStateOf(false) }
 
-    Scaffold(
-        topBar = {
-            SellerProfileTopBar(
-                onBackClick = {},
-                onShareClick = {},
-                onMenuClick = {}
-            )
-        },
-        contentColor = CarrotTheme.colors.gray1,
-        content = { innerPadding ->
-            Box(modifier = Modifier.fillMaxSize()) {
-                SellerProfileContent(
-                    modifier = Modifier.padding(innerPadding),
-                    onFollowClick = { isDialogVisible = true }
-                )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(CarrotTheme.colors.white)
+    ) {
+        SellerProfileTopBar(
+            onBackClick = {},
+            onShareClick = {},
+            onMenuClick = {}
+        )
 
-                if (isDialogVisible) {
-                    SellerFollowDialog(
-                        onClickCancel = { isDialogVisible = false }
-                    )
-                }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .zIndex(1f)
+        ) {
+            if (isDialogVisible) {
+                SellerFollowDialog(
+                    onClickCancel = { isDialogVisible = false }
+                )
             }
+
+            SellerProfileContent(
+                onFollowClick = { isDialogVisible = true }
+            )
         }
-    )
+    }
 }
 
 @Composable
 private fun SellerProfileContent(
-    modifier: Modifier,
     onFollowClick: () -> Unit
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize()
     ) {
         item {
             SellerProfileSection(
