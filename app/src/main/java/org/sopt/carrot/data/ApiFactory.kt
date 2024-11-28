f7e6cd1d64bec548e51d180b37adfd0122f1f4a0
wjdrjs00 <kimjk3409@gmail.com>
2024-11-28 15:27:59 +0900

    [Feat/#23] ApiFactory 기능 추가
    - json 설정 추가
    - jsonConverter 분리
    - loggingInterceptor 메시지 타입 분기 처리
    - Debug 여부에 따른 로깅 레벨 설정 처리

--- app/src/main/java/org/sopt/carrot/data/ApiFactory.kt
+++ app/src/main/java/org/sopt/carrot/data/ApiFactory.kt
@@ -2,18 +2,43 @@ package org.sopt.carrot.data
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONArray
+import org.json.JSONObject
 import org.sopt.carrot.BuildConfig
+import org.sopt.carrot.core.extension.isJsonArray
+import org.sopt.carrot.core.extension.isJsonObject
+import org.sopt.carrot.data.service.CategoryService
 import org.sopt.carrot.data.service.DummyService
+import retrofit2.Converter
 import retrofit2.Retrofit
+import timber.log.Timber
 
 object ApiFactory {
     private const val BASE_URL: String = BuildConfig.BASE_URL
+    private const val OKHTTP = "okhttp"
+    private const val UNIT_TAB = 4
 
-    private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
+    private val json = Json {
+        ignoreUnknownKeys = true
+        prettyPrint = true
+    }
+
+    private val jsonConverter: Converter.Factory =
+        json.asConverterFactory("application/json".toMediaType())
+
+    private val loggingInterceptor: Interceptor = HttpLoggingInterceptor { message ->
+        when {
+            message.isJsonObject() -> Timber.tag(OKHTTP).d(JSONObject(message).toString(UNIT_TAB))
+            message.isJsonArray() -> Timber.tag(OKHTTP).d(JSONArray(message).toString(UNIT_TAB))
+            else -> Timber.tag(OKHTTP).d("CONNECTION INFO -> $message")
+        }
+    }.apply {
+        level =
+            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
     }
 
     private val client = OkHttpClient.Builder()
@@ -24,7 +49,7 @@ object ApiFactory {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(client)
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+            .addConverterFactory(jsonConverter)
             .build()
     }
 
@@ -33,4 +58,5 @@ object ApiFactory {
 
 object ServicePool {
     val dummyService by lazy { ApiFactory.create<DummyService>() }
+    val categoryService by lazy { ApiFactory.create<CategoryService>() }
 }
